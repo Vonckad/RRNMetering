9f33df6ac053178c333534be766dc725ab28055a
Vonckad <Vonkad@yandex.ru>
2021-07-01 11:24:46 +0300

    Initial Commit

---
+++ RRNMetering/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RRNMetering
+//
+//  Created by Vlad Ralovich on 1.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
