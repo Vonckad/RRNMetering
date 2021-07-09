3015b11d01ffe329554e1bd3ce9f82af18381c77
Vonckad <Vonkad@yandex.ru>
2021-07-09 13:54:14 +0300

    C модемом все работает

--- RRNMetering/DetailViewController.swift
+++ RRNMetering/DetailViewController.swift
@@ -6,95 +6,53 @@
 //
 
 import UIKit
-import Alamofire
+import Kanna
 import WebKit
 
-class DetailViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler {
-    
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        if(message.name == "callbackHandler") {
-                    guard let body = message.body as? [String: Any] else {
-                        print("could not convert message body to dictionary: \(message.body)")
-                        return
-                    }
-
-                    guard let payload = body["payload"] as? String else {
-                        print("Could not locate payload param in callback request")
-                        return
-                    }
-
-                    print(payload)
-        }
-    }
-    
+class DetailViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var txLabel: UILabel!
-    @IBOutlet weak var rxLabel: UILabel!
+    @IBOutlet weak var deviceTableView: UITableView!
     @IBOutlet weak var ipLabel: UILabel!
     @IBOutlet weak var updateButtonOutlet: UIButton!
-    
     @IBOutlet weak var myWebView: WKWebView!
+    @IBOutlet weak var webViewSwitch: UISwitch!
+    
     
     var titleForLabel = ""
     var ipPost = ""
     let activiti = UIActivityIndicatorView()
-    
-    
+    var deviceArray: Array<String> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         myWebView.uiDelegate = self
         myWebView.navigationDelegate = self
+        deviceTableView.delegate = self
+        deviceTableView.dataSource = self
+        
+        myWebView.isHidden = true
         
         view.addSubview(activiti)
         activiti.translatesAutoresizingMaskIntoConstraints = false
         activiti.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         activiti.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        activiti.isHidden = true
+        activiti.isHidden = false
+        activiti.startAnimating()
+        updateButtonOutlet.isHidden = true
         
         titleLabel.text = titleForLabel
         ipLabel.text = ipPost
         load()
-
-        
-        
-       
+        activitiSetup()
     }
 
     func load() {
         
         let request = URLRequest(url: URL(string: "http://192.168.1.1")!)
         
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            guard
-                let url = response?.url,
-                let httpResponse = response as? HTTPURLResponse,
-                let fields = httpResponse.allHeaderFields as? [String: String]
-            else { return }
-
-            let cookies = HTTPCookie.cookies(withResponseHeaderFields: fields, for: url)
-            HTTPCookieStorage.shared.setCookies(cookies, for: url, mainDocumentURL: nil)
-            for cookie in cookies {
-                var cookieProperties = [HTTPCookiePropertyKey: Any]()
-                cookieProperties[.name] = cookie.name
-                cookieProperties[.value] = cookie.value
-                cookieProperties[.domain] = cookie.domain
-                cookieProperties[.path] = cookie.path
-                cookieProperties[.version] = cookie.version
-                cookieProperties[.expires] = Date().addingTimeInterval(31536000)
-
-                let newCookie = HTTPCookie(properties: cookieProperties)
-                HTTPCookieStorage.shared.setCookie(newCookie!)
-
-                print("name: \(cookie.name) value: \(cookie.value)")
-            }
-        }
-        .resume()
-        
         myWebView.load(request)
-        
         
         let javaScript = """
         $(document).ready(function() {
@@ -113,47 +71,98 @@ class DetailViewController: UIViewController, WKUIDelegate, WKNavigationDelegate
         
         myWebView.configuration.userContentController.add(self, name: "callbackHandler")
         myWebView.configuration.userContentController.addUserScript(script)
-
         
     }
     
     @IBAction func updateAtcion(_ sender: Any) {
-       activitiSetup()
-        
-        
+        deviceArray = []
+        activitiSetup()
         
     }
-    
+        
     func activitiSetup() {
         activiti.isHidden = false
         self.updateButtonOutlet.isHidden = true
+        self.deviceTableView.isHidden = true
+        self.webViewSwitch.isHidden = true
+        self.myWebView.isHidden = true
+        
         activiti.startAnimating()
         DispatchQueue.global().asyncAfter(deadline: .now() + 1, execute: {
             DispatchQueue.main.async { [weak self] in
                 self?.updateButtonOutlet.isHidden = false
+                self?.deviceTableView.isHidden = false
+                self?.webViewSwitch.isHidden = false
+                self?.webviewHiden()
                 self?.activiti.stopAnimating()
                 self?.activiti.isHidden = true
-                
-                self?.myWebView.load(URLRequest(url: URL(string: "https://192.168.1.1/html/advance.html#arp_table")!))
-                self?.myWebView.reload()
+                let urlTable = URL(string: "http://192.168.1.1/html/advance.html#arp_table")
+                self?.myWebView.load(URLRequest(url: urlTable!))
             }
         })
     }
-}
-
-
-struct Root: Codable {
-//    var csrf: Csrf
-    var data: MyData
-}
-
-struct Csrf: Codable {
-    var csrf_param: String
-    var csrf_token: String
     
+    func webviewHiden() {
+        if webViewSwitch.isOn {
+            myWebView.isHidden = false
+        } else {
+            myWebView.isHidden = true
+        }
+    }
+    
+    @IBAction func webViewSwitchAction(_ sender: Any) {
+        webviewHiden()
+    }
 }
 
-struct MyData: Codable {
-    var UserName: String
-    var Password: String
+extension DetailViewController: WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler {
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
+            DispatchQueue.main.async { [weak self] in
+                self?.myWebView.evaluateJavaScript("arp_table_info_view_data_edit.children[0].children[0].children[1].innerHTML") { res, error in
+                   
+                    if res != nil {
+                        let ppp: String = res as! String
+                        
+                        if let kanna = try? Kanna.HTML(html: ppp, encoding: String.Encoding.utf8) {
+                            
+                            for (index, link) in kanna.xpath("//td").enumerated() {
+                                
+                                if index == 0 || index % 5 == 0 {
+                                    self?.deviceArray.append(link.text!)
+                                }
+                            }
+                            self?.deviceTableView.reloadData()
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if(message.name == "callbackHandler") {
+            guard let body = message.body as? [String: Any] else {
+                return
+            }
+            
+            guard (body["payload"] as? String) != nil else {
+                return
+            }
+        }
+    }
+}
+
+extension DetailViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return deviceArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = deviceArray[indexPath.row]
+        return cell
+    }
 }
